04ab67dba21850fba3c1209faf9fe35d290949b1
RomaBortnik <bortnik25051998@gmail.com>
2023-02-18 19:48:11 +0200

    task #1, 3 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+gallery.innerHTML = markup;
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackFormInput = document.querySelector('.feedback-form input');
+const feedbackFormTextarea = document.querySelector('.feedback-form textarea');
 
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onInputChange, 500));
+
+updateForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (feedbackFormTextarea.value === '' || feedbackFormInput.value === '') {
+    return;
+  }
+
+  console.log(updateForm());
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onInputChange(event) {
+  //   formData.email = feedbackFormInput.value.trim();
+  //   formData.message = feedbackFormTextarea.value.trim();
+  //   return localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  formData[event.target.name] = event.target.value;
+  return localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function updateForm() {
+  const parsedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (parsedData) {
+    (feedbackFormInput.value = parsedData.email || ''),
+      (feedbackFormTextarea.value = parsedData.message || '');
+  }
+  return parsedData;
+}
